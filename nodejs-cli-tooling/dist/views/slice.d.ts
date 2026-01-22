d39804e1c0b5e48d4d516ca98bf8338f7883c8c7
Sebastian Bortz <sebastianbortz@gmail.com>
2026-01-22 11:05:01 +0100

    Add Node.js/TypeScript CLI tooling, fix .gitignore

--- nodejs-cli-tooling/dist/views/slice.d.ts
+++
@@ -1,5 +0,0 @@
-import { InformationFlowModel } from '../models/types.js';
-/**
- * Render the slice view - detailed panels for each slice
- */
-export declare function renderSlice(model: InformationFlowModel): void;
--- nodejs-cli-tooling/dist/views/table.d.ts
+++
@@ -1,5 +0,0 @@
-import { InformationFlowModel } from '../models/types.js';
-/**
- * Render the table view - tabular overview with data flow tree
- */
-export declare function renderTable(model: InformationFlowModel): void;
